ba7b5141368697c617d4e0882d9340396ba6c529
Morfiadakis Emmanouil <manosmorf97@gmail.com>
2022-08-20 21:59:27 +0300

    Great

--- Spark_Queries/src/main/java/com/spark/example/App.java
+++ Spark_Queries/src/main/java/com/spark/example/App.java
@@ -1,7 +1,10 @@
 package com.spark.example;
 
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.storage.StorageLevel;
@@ -12,9 +15,9 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class App {
-
 	public static void main(String[] args) {
 		System.out.println("Ready");
 		String path="/home/manos/Μεταπτυχιακό/1o Εξάμηνο/"
@@ -23,48 +26,63 @@ public class App {
 		SparkSession session=SparkSession.
 				builder()
 				.appName("Spark_Queries")
-				.master("local[4]")
+				.master("local[*]")
 				.getOrCreate();
 		try(JavaSparkContext context=new 
 				JavaSparkContext(session.sparkContext())){
-			System.out.println("Top 25 most viewed movies");
 			//ArrayList<Movie> movies=new ArrayList<Movie>();
 			JavaRDD<String> movie_lines=context.
 					textFile(path+"ml-10m/ml-10M100K/movies.dat");
-			movie_lines=movie_lines.persist(StorageLevel.MEMORY_ONLY( )) ;
+			JavaPairRDD<String,String> movie_info=movie_lines.mapToPair(
+					line->new Tuple2<String,String>(
+							line.substring(0,line.indexOf(':')),
+								line.replace(':',' ')));
+			movie_info=movie_info.persist(StorageLevel.DISK_ONLY( )) ;
 			JavaRDD<String> rating_lines=context.
 					textFile(path+"ml-10m/ml-10M100K/ratings.dat");
-			//JavaRDD<Movie> movies=movie_lines.map(
-				//	line->createMovie(line));
+		
+			
+			
 			JavaPairRDD<String,Integer>movie_1_rdd=rating_lines.mapToPair( 
 					line->new Tuple2<String,Integer>(takeMovieId(line),1));
 			JavaPairRDD<String,Integer> movie_views_rdd=movie_1_rdd.
 					reduceByKey (( a , b)-> a + b);
+			movie_views_rdd=movie_views_rdd.persist(StorageLevel.DISK_ONLY( ));
 			JavaPairRDD<Integer,String> views_movie_rdd=movie_views_rdd.
 					mapToPair(mvr->
-					new Tuple2<Integer,String>(mvr._2(),mvr._1()));
+						new Tuple2<Integer,String>(mvr._2(),mvr._1()));
+			//views_movie_rdd=views_movie_rdd.persist(StorageLevel.DISK_ONLY( ));
 			JavaPairRDD<Integer,String>sorted_vmrdd=views_movie_rdd.
 					sortByKey(false);
-			JavaPairRDD<String,Integer>sorted_mvrdd=sorted_vmrdd.mapToPair(
-					s_vmrdd->new Tuple2<String,Integer>(
-							s_vmrdd._2(),s_vmrdd._1()));
-			sorted_mvrdd=sorted_mvrdd.persist(StorageLevel.MEMORY_ONLY( )) ;
-			JavaPairRDD<String,String> movie_info=movie_lines.mapToPair(
-					line->new Tuple2<String,String>(
-							line.substring(line.indexOf(':')),
-								line.replace(':',' ')));
-			JavaPairRDD<String, Tuple2<Integer,String>> most_viewed =
-					sorted_mvrdd.join(movie_info);
-			List<Tuple2<String, Tuple2<Integer, String>>> results = 
-				most_viewed.take(25);
-			results.forEach(
-					res->System.out.println("------------------------"));
+			List<String> top_movies=sorted_vmrdd.take(25).stream().
+					map(res->res._2).
+					collect(Collectors.toList());
+			JavaPairRDD<String,String> top_movies_rdd=
+					context.parallelize(top_movies).mapToPair(
+							id->new Tuple2<String,String>(id," "));
+			JavaPairRDD<String,Tuple2<String,String>>results_rdd=
+					top_movies_rdd.join(movie_info);
+			List<Tuple2<String,Tuple2<String,String>>> results=
+					results_rdd.take(25);
+			List<String> results_2_2=(ArrayList<String>)results.stream()
+					.map(res->res._2._2).
+					collect(Collectors.toList());
+			Collections.sort(top_movies);
+			Collections.sort(results_2_2);
+			System.out.println("---------------------------------");
+			System.out.println("Top 25 most viewed movies");
+			results_2_2.forEach(
+					res->System.out.println(res));
+			top_movies.forEach(
+					movie->System.out.println(movie));
 			System.out.println("Done.Press any key to  continue");
-			String end=new Scanner(System.in).nextLine();
-			System.out.println(end);
+			Scanner scanner=new Scanner(System.in);
+			scanner.nextLine();
+			scanner.close();
 			//join with movies.dat
 			//print results
 		    //write your code
+			context.close();
 			
 			
 			
